3bdf0cd76e657875fe03c4d34f5f682a96073ffb
LuisaSanto <luisa.santo@gmail.com>
2019-02-23 00:28:10 +0000

    Add endGameStatus

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var topButton: UIButton!         // Has TAG = 1
     @IBOutlet weak var bottomButton: UIButton!      // Has TAG = 2
     @IBOutlet weak var storyTextView: UILabel!
+    @IBOutlet weak var restartButton: UIButton!
     
     var path = PathLine()
     
@@ -27,15 +28,21 @@ class ViewController: UIViewController {
             if (path.root?.children[0].children.isEmpty == false) {
                 updateUI(position: 0)
             } else {
-                removeButton()
+                endStory()
             }
         } else {
             if (path.root?.children[1].children.isEmpty == false) {
                 updateUI(position: 1)
             } else {
-                removeButton()
+                endStory()
             }
         }
+    }
+    
+    @IBAction func restartPressed(_ sender: UIButton) {
+        initialization()
+        bottomButton.isHidden = false
+        topButton.isHidden = false
     }
     
     func initialization() {
@@ -43,6 +50,7 @@ class ViewController: UIViewController {
         storyTextView.text = path.root?.value.text
         topButton.setTitle(path.root?.value.answer1, for: UIControl.State.normal)
         bottomButton.setTitle(path.root?.value.answer2, for: UIControl.State.normal)
+        restartButton.isHidden = true
     }
     
     func updateUI(position: Int) {
@@ -62,10 +70,9 @@ class ViewController: UIViewController {
         bottomButton.isHidden = true
     }
     
-    func restart () {
-        initialization()
-        bottomButton.isHidden = false
-        topButton.isHidden = false
+    func endStory() {
+        removeButton()
+        restartButton.isHidden = false
     }
     
 }
